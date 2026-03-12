24ee780154a55dd46885aef07c30525bc00b8b5e
Andrei Udalov <137709149+AndreiUdalovIT@users.noreply.github.com>
2026-03-13 02:19:12 +0300

    unit

--- index.js
+++ index.js
@@ -3,18 +3,59 @@ let posts = [];
 const title = document.querySelector('.js__title__input');
 const text = document.querySelector('.js__text__input');
 const button = document.querySelector('.js__send__btn');
-const post = document.querySelector('.js-post');
+const post = document.querySelector('.js__post');
+const error = document.querySelector('.js__error');
+
+let errorHTML = '';
 
 button.addEventListener('click', function () {
-    const getDate = new Date();
-    const getTitle = title.value;
-    const getText = text.value;
+    if (titleError() === false) {
+        if (textError() === false) {
+            getValue();
+            sendPost();
+            clearValue();}
+    }
+})
+
+
+function titleError() {
+    let titleLength = title.value.length;
+        
+    if ((titleLength > 100) || (titleLength === 0)) {
+        errorHTML = `<p class="error__text">Заголовок больше 100 символов или пустой</p>`
+        error.innerHTML = errorHTML;
+        } else {
+        errorHTML = '';
+        error.innerHTML = errorHTML;
+        return false;
+    }
+}
+
+function textError() {
+    let textLength = text.value.length;
+        
+    if ((textLength > 200) || (textLength === 0)) {
+        errorHTML = `<p class="error__text">Пост больше 200 символов или пустой</p>`
+        error.innerHTML = errorHTML;
+        } else {
+        errorHTML = '';
+        error.innerHTML = errorHTML;
+        return false;
+    }
+}
+
+function getValue() {
+    const dateValue = new Date();
+    const titleValue = title.value;
+    const textValue = text.value;
+        
     posts.push({
-        date: `${getDate.toLocaleDateString('ru-RU')} ${getDate.toLocaleTimeString()}`,
-        title: getTitle,
-        text: getText,});
-    
-    
+        date: `${dateValue.toLocaleDateString('ru-RU')} ${dateValue.toLocaleTimeString()}`,
+        title: titleValue,
+        text: textValue,});
+}
+
+function sendPost() {
     let postHTML = '';
 
     for (let i = 0; i < posts.length; i++) {
@@ -24,11 +65,13 @@ button.addEventListener('click', function () {
                         <p class="title__post">${posts[i].title}</p>
                         <p class="text__post">${posts[i].text}</p>
                     </div>`
-        
+            
     }
-    
+        
     post.innerHTML = postHTML
-
+}
+    
+function clearValue() {
     title.value = '';
     text.value = '';
-})
+}
